023b71ef97d673f0072b2fa57e0ae9ad5a7383ba
deaxparadox <nitish136900kushwaha@gmail.com>
2024-04-02 14:33:57 +0530

    library added

---
+++ 106-library/multiprocessing-lib/code/102-simple-factorial.py
@@ -0,0 +1,24 @@
+import os
+from multiprocessing import Pool
+
+# get total cpu available
+CPU_COUNT = os.cpu_count()
+
+def __calculate_factorial(x: int) -> int:
+    if x == 1:
+        return 1
+    return x * __calculate_factorial(x-1)  
+
+def factorial(x: int) -> None:
+    _value = __calculate_factorial(x)
+    print(f"Value of {x} is {_value}")
+    return _value
+
+
+
+def f(x):
+    return x * x
+
+if __name__ == "__main__":
+    with Pool(CPU_COUNT) as p:
+        print(p.map(factorial, [x+40 for x in [1, 2, 3]]))
